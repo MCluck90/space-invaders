b010fef973193ff416244015714739001a6ed191
Mike C <mcluck90@gmail.com>
2015-05-12 15:39:21 -0600

    Added part 3 - controls

---
+++ 03/factory.js
@@ -0,0 +1,67 @@
+(function() {
+
+var World = Psykick2D.World,
+    Sprite = Psykick2D.Components.GFX.Sprite;
+
+/**
+ * Converts degrees to radians
+ * @param {number} deg
+ */
+function degToRad(deg) {
+  return (deg * Math.PI) / 180;
+}
+
+/**
+ * Helper function for creating enemies
+ * @param {number} num  Determines which color to generate
+ * @param {number} x    X position
+ * @param {number} y    Y position
+ */
+function createEnemy(num, x, y) {
+  var enemy = World.createEntity(),
+      sprite = new Sprite({
+        frameName: 'enemy' + num + '-1',
+        x: x,
+        y: y,
+        width: 40,
+        height: 30
+      });
+
+  enemy.addComponent(sprite);
+  return enemy;
+}
+
+Game.Factory = {
+  createPlayer: function(x, y) {
+    var player = World.createEntity(),
+        sprite = new Sprite({
+          frameName: 'player-1',
+          x: x,
+          y: y,
+          width: 64,
+          height: 29,
+          rotation: degToRad(270)
+        });
+
+    player.addComponent(sprite);
+    return player;
+  },
+
+  createOrangeEnemy: function(x, y) {
+    return createEnemy(1, x, y);
+  },
+
+  createPurpleEnemy: function(x, y) {
+    return createEnemy(2, x, y);
+  },
+
+  createBlueEnemy: function(x, y) {
+    return createEnemy(3, x, y);
+  },
+
+  createYellowEnemy: function(x, y) {
+    return createEnemy(4, x, y);
+  }
+};
+
+})();
---
+++ 03/main.js
@@ -0,0 +1,47 @@
+(function() {
+
+var World = Psykick2D.World,
+    Factory = Game.Factory,
+    SpriteSystem = Psykick2D.Systems.Render.Sprite,
+    PlayerControl = Game.PlayerControl;
+
+// Initialize the world
+World.init({
+  width: 350,
+  height: 500,
+  backgroundColor: '#000',
+  preload: {
+    spriteSheets: [
+      '../sprites/player.json',
+      '../sprites/enemy1.json',
+      '../sprites/enemy2.json',
+      '../sprites/enemy3.json',
+      '../sprites/enemy4.json',
+    ]
+  }
+});
+
+var mainLayer = World.createLayer();
+
+var spriteSystem = new SpriteSystem(),
+    controlSystem = new PlayerControl();
+
+// Give the player some controls and draw them
+var player = Factory.createPlayer(161, 480);
+controlSystem.player = player;
+spriteSystem.addEntity(player);
+
+// Add some enemies to the screen
+spriteSystem.addEntity(Factory.createOrangeEnemy(20, 20));
+spriteSystem.addEntity(Factory.createPurpleEnemy(100, 20));
+spriteSystem.addEntity(Factory.createBlueEnemy(180, 20));
+spriteSystem.addEntity(Factory.createYellowEnemy(260, 20));
+
+// Add all of the systems to the layer
+mainLayer.addSystem(controlSystem);
+mainLayer.addSystem(spriteSystem);
+
+// Add the layer to the world to start running it
+World.pushLayer(mainLayer);
+
+})();
